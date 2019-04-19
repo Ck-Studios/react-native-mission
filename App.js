410041c1d2a2ad0ce053262f6a3bdfbd4895c5a7
ck <ck@devhi.me>
2019-04-19 10:58:45 +0900

    Reducer init.

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import reducer from "./src/Reducer";
 import {AppContainer} from "./src/application/navigation/Navigator";
+
+const store = createStore(reducer);
 
 export default class App extends Component<Props> {
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <AppContainer/>
-      </SafeAreaView>
+      <Provider store={store}>
+        <SafeAreaView style={styles.container}>
+          <AppContainer/>
+        </SafeAreaView>
+      </Provider>
     );
   }
 }
--- src/application/navigation/Navigator.js
+++ src/application/navigation/Navigator.js
@@ -1,37 +1,65 @@
+import React from "react";
 import {
   createAppContainer,
   createStackNavigator,
   createMaterialTopTabNavigator,
 } from "react-navigation";
+import Icon from "react-native-vector-icons/AntDesign";
 import {Dimensions, StyleSheet, Image} from "react-native";
 import ProductContainer from "../../products/containers/ProductContainer";
 import WishListContainer from "../../wishlist/containers/WishListContainer";
 
 const {width, height} = Dimensions.get('window');
 const ICON_SIZE = 25;
-const tabBarOptions = {
-  showIcon: false,
-  showLabel: true,
-  style: {
-    backgroundColor: '#ffffff',
-  },
-  indicatorStyle: {
-    opacity: 1,
-  },
-};
+// const tabBarOptions = {
+//   showIcon: true,
+//   showLabel: false,
+//   style: {
+//     color: '#000000',
+//     backgroundColor: '#ffffff',
+//   },
+//   indicatorStyle: {
+//     opacity: 0,
+//   },
+// };
 
 export const RootTab = createMaterialTopTabNavigator({
   ProductContainer: {
-    screen: ProductContainer
+    screen: ProductContainer,
+    navigationOptions: {
+      tabBarIcon: ({focused}) => (
+        focused ?
+          <Icon name={'profile'} size={ICON_SIZE} color={'#ff24b2'}/>
+          :
+          <Icon name={'profile'} size={ICON_SIZE} color={'#787878'}/>
+      )
+    }
   },
 
   WishListContainer: {
-    screen: WishListContainer
+    screen: WishListContainer,
+    navigationOptions: {
+      tabBarIcon: ({focused}) => (
+        focused ?
+          <Icon name={'shoppingcart'} size={ICON_SIZE} color={'#ff24b2'}/>
+          :
+          <Icon name={'shoppingcart'} size={ICON_SIZE} color={'#787878'}/>
+      )
+    }
   }
 }, {
   swipeEnabled: true,
   tabBarPosition: 'bottom',
-  tabBarOptions,
+  tabBarOptions: {
+    showIcon: true,
+    showLabel: false,
+    style: {
+      backgroundColor: '#ffffff',
+    },
+    indicatorStyle: {
+      opacity: 0,
+    },
+  },
 });
 
 export const AppNavigator = createStackNavigator({
